54d3506d9e7693d45f1d1cd85c732a993d45b5a4
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-04-30 01:34:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ btn.addEventListener("click", function comprobar() {
   let text = document.getElementById("mensaje");
   let inpu = document.getElementById("numero").value;
   let random = Math.floor(Math.random() * 10) + 1;
-  if (parseInt(inpu) == random) {
+  parseInt(inpu);
+  if (inpu > 10) {
+    text.innerHTML = "El número debe ser menor que 10";
+    text.style.color = "red";
+  }else if (inpu < 0) {
+    text.innerHTML = "El número debe ser mayor que 0";
+    text.style.color = "red";
+    
+  } else if (inpu == random) {
     text.innerHTML = "¡Has acertado!";
     text.style.color = "green";
   } else {
